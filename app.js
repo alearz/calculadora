e2f94113d066a7ff9b9f5bcffe3804a41f1dc9e1
Alexis Arzamendia <arzalexis7@gmail.com>
2020-12-23 09:14:08 -0300

    calculadora

---
+++ app.js
@@ -0,0 +1,3 @@
+let calculadora =  require('.calculadora')
+
+console.log(calculadora.sumar(2,5))
---
+++ calculadora.js
@@ -0,0 +1,7 @@
+let calculadora = {
+    sumar : (a,b) => a + b,
+    restar : (a,b) => a - b,
+    multiplicar : (a,b) => a * b
+}
+
+module.exports = calculadora // exportando como modulo
